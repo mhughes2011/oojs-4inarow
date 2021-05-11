affb911d0c00eec929171bfda7efe233a6bbb282
M Hughes <mhughes2011@gmail.com>
2021-05-10 20:30:19 -0700

    added move left and right functions to game and token

--- js/Token.js
+++ js/Token.js
@@ -3,6 +3,23 @@ class Token {
         this.owner = owner;
         this.id = `token-${index}-${owner.id}`;
         this.dropped = false;
+        this.columnLocation = 0;
+    }
+
+    /**
+     * Gets associated htmlToken.
+     * @return {element} HTML element associated with the token object
+     */
+    get htmlToken() {
+        return document.getElementById(this.id);
+    }
+
+    /** 
+     * Gets left offset of html element.
+     * @return  {number}   Left offset of token object's htmlToken.
+     */
+    get offsetLeft() {
+        return this.htmlToken.offsetLeft;
     }
 
     // This is the render method for the new html tokens
@@ -14,4 +31,25 @@ class Token {
         token.style.backgroundColor = this.owner.color;
     }
 
+
+    /** 
+     * Moves html token one column to left.
+     */
+    moveLeft() {
+        if(this.columnLocation > 0) {
+            this.htmlToken.style.left = this.offsetLeft - 76;
+            this.columnLocation -= 1;
+        }
+    }
+
+    /** 
+     * Moves html token one column to right
+     * @param   {number}    columns - number of columns on the game board    
+     */
+    moveRight(columns) {
+        if(this.columnLocation < columns - 1) {
+            this.htmlToken.style.left = this.offsetLeft + 76;
+            this.columnLocation += 1;
+        }
+    }
 }
